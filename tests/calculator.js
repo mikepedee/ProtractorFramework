d777f441cd5660627adb44c43cea5fa6b7b3d133
Michael Pedee <mpedee@sae.org>
2020-05-08 10:39:34 -0400

    Removed errored test

--- tests/calculator.js
+++ tests/calculator.js
@@ -8,13 +8,4 @@ describe('demo calculator tests', function () {
     homepage.verifyResult('7');
     browser.sleep(2000);
   });
-
-  it('subtraction test', function () {
-    homepage.get('http://juliemr.github.io/protractor-demo/');
-    homepage.enterFirstNumber('4');
-    homepage.enterSecondNumber('3');
-    homepage.clickGo1();
-    homepage.verifyResult('7');
-    browser.sleep(2000);
-  });
 });
